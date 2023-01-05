7e9723df8a27a1bf9342ca09ae908999c34a99ca
Kudo Rintaro <119265020+KudoRintaro@users.noreply.github.com>
2023-01-05 15:37:32 +0000

    フォロー機能の非同期通信化

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @book_comment=BookComment.new
     @book_comments=@book.book_comments
+    @user=User.find(@book.user.id)
   end
 
   def index
